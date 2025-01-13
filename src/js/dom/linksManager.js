53db353a8ebdb9307c5324ef5866ddc92db33807
Aleksa Petrovic <aleksapetrovic2907@gmail.com>
2025-01-13 20:17:15 +0100

    add JSDoc to linksManager.trySelect()

--- src/js/dom/linksManager.js
+++ src/js/dom/linksManager.js
@@ -1,7 +1,14 @@
 let selectedLink = null;
 
-export default function select(link) {
-    if (link === selectedLink) return;
+/**
+ * Attempts to select a given link element by adding a "selected" class to it.
+ * If another link is already selected, it removes the "selected" class from that link.
+ * 
+ * @param {HTMLElement} link - The link element to be selected.
+ * @returns {boolean} - Returns `true` if the link was successfully selected, or `false` if the given link was already selected.
+ */
+export default function trySelect(link) {
+    if (link === selectedLink) return false;
 
     if (selectedLink) {
         selectedLink.classList.remove("selected");
@@ -9,4 +16,5 @@ export default function select(link) {
 
     selectedLink = link;
     selectedLink.classList.add("selected");
+    return true;
 }
