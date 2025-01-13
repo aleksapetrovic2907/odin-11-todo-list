eb70cd7818682a4655ea8eebf414aede1e0d88db
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-13 18:55:54 +0100

    generate base layout in nav.css

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import 'normalize.css'
 import '../css/styles.css'
+import '../css/nav.css'
 
 import LocalStorageService from "./services/localStorageService.js";
 import ProjectsService from "./services/projectsService.js";
