2c625868f9543b6883593c1fd29bc78cde0747ef
Aleksa Petrovic <aleksapetrovic2907@gmail.com>
2025-01-13 21:05:19 +0100

    implement filterer for filtering projects and tasks based on date

---
+++ src/js/dom/filterer.js
@@ -0,0 +1,21 @@
+import { isToday, isFuture, parseISO } from "date-fns";
+
+function filterAll(projects) {
+    return projects;
+}
+
+function filterTodays(projects) {
+    return projects.filter((project) =>
+        project.tasks.some((task) => isToday(parseISO(task.dueDate)))
+    );
+}
+
+function filterUpcoming(projects) {
+    return projects.filter((project) =>
+        project.tasks.some(
+            (task) => isToday(parseISO(task.dueDate)) || isFuture(parseISO(task.dueDate))
+        )
+    );
+}
+
+export { filterAll, filterTodays, filterUpcoming };
