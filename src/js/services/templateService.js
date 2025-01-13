ba8e62c0a5ef17ebc03eafb17121de6f9a37628d
Aleksa Petrovic <aleksapetrovic2907@gmail.com>
2025-01-13 17:46:24 +0100

    implement TemplateService for replacing placeholders with values

---
+++ src/js/services/templateService.js
@@ -0,0 +1,18 @@
+export default class TemplateService {
+    /**
+     * Replaces placeholders in the template with provided values.
+     * @param {string} template - The HTML template with placeholders (e.g., {{name}})
+     * @param {Object} data - Key-value pairs where keys correspond to placeholders
+     * @returns {string} - The updated template with replaced values
+     */
+    render(template, data) {
+        let renderedTemplate = template;
+
+        Object.keys(data).forEach(key => {
+            const regex = new RegExp(`{{${key}}}`, 'g');
+            renderedTemplate = renderedTemplate.replace(regex, data[key]);
+        });
+
+        return renderedTemplate;
+    }
+}
