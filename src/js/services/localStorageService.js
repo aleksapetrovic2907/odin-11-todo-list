9d5bee59bba0c8f2982c77dacc2bdb8437eff914
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-13 16:58:06 +0100

    implement LocalStorageService

---
+++ src/js/services/localStorageService.js
@@ -0,0 +1,21 @@
+class LocalStorageService {
+    save(key, data) {
+        localStorage.setItem(key, JSON.stringify(data));
+    }
+
+    load(key) {
+        const data = localStorage.getItem(key);
+        return data ? JSON.parse(data) : null;
+    }
+
+    remove(key) {
+        localStorage.removeItem(key);
+    }
+
+    clear() {
+        localStorage.clear();
+    }
+}
+
+const localStorageService = new LocalStorageService();
+export default localStorageService;
