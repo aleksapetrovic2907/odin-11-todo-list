45235c7a115998f59cf06fcad916e15ca75f76c5
Aleksa Petrovic <aleksapetrovic2907@gmail.com>
2025-01-13 20:22:47 +0100

    selecting projectLink invokes a custom event

--- src/js/dom/projectLinks.js
+++ src/js/dom/projectLinks.js
@@ -2,6 +2,7 @@ import projectLinkTemplateUrl from "../../html/user-panel/project-link.html";
 
 import { projectsService } from "../index.js";
 import TemplateService from "../services/templateService.js";
+import trySelect from "./linksManager.js";
 
 const projectLinks = document.querySelector(".project-links");
 
@@ -12,6 +13,13 @@ function refreshProjectLinks() {
     projects.forEach(p => {
         const data = { name: p.name, };
         const projectLink = TemplateService.render(projectLinkTemplateUrl, data);
+        projectLink.addEventListener(() => {
+            if (trySelect(projectLink)) {
+                const event = new CustomEvent("projectLinkSelected", {
+                    detail: { projectId: p.id },
+                });
+            }
+        });
         projectLinks.appendChild(projectLink);
     });
 }
