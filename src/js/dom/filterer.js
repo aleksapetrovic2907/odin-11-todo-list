1f4171b4a4cc0371a4c4048bb50549653507008b
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-14 16:53:03 +0100

    filterer filters out projects without any tasks

--- src/js/dom/filterer.js
+++ src/js/dom/filterer.js
@@ -1,20 +1,18 @@
 import { isToday, isFuture, parseISO } from "date-fns";
 
 function filterAll(projects) {
-    return projects;
+    return projects.filter((project) => project.tasks.length > 0);
 }
 
 function filterTodays(projects) {
     return projects.filter((project) =>
-        project.tasks.some((task) => isToday(parseISO(task.dueDate)))
+        project.tasks.length > 0 && project.tasks.some((task) => isToday(parseISO(task.dueDate)))
     );
 }
 
 function filterUpcoming(projects) {
     return projects.filter((project) =>
-        project.tasks.some(
-            (task) => isFuture(parseISO(task.dueDate))
-        )
+        project.tasks.length > 0 && project.tasks.some((task) => isFuture(parseISO(task.dueDate)))
     );
 }
 
