7c251441dc025a39ad3bf0ee239c0d78250a848f
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-01-16 18:20:59 +0100

    invoke selectFilter on app start

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,12 @@ import '../css/nav.css'
 import LocalStorageService from "./services/localStorageService.js";
 import ProjectsService from "./services/projectsService.js";
 
+import { selectFilter } from "./dom/filterLinks.js";
+
 const storageService = new LocalStorageService();
 const projectsService = new ProjectsService(storageService);
-
 window.onbeforeunload = () => { projectsService.saveProjectsToStorage() };
+
+selectFilter("all");
 
 export { projectsService };
